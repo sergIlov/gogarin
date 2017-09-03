ab0137a6f843c3002b37bb2504e972a76f5f4ac2
Anton Kuzmenko <antonkuzmenko.dev@gmail.com>
2017-09-03 20:40:22 +0300

    Add timeouts to all network calls in the redis.Connection

--- pkg/transport/redis/connection.go
+++ pkg/transport/redis/connection.go
@@ -15,9 +15,15 @@ func New(c Config) transport.Connection {
 	pool := &redis.Pool{
 		MaxActive:   c.MaxActiveConnections,
 		MaxIdle:     c.MaxIdleConnections,
-		IdleTimeout: time.Duration(c.ConnectionIdleTimeoutInSeconds) * time.Second,
+		IdleTimeout: time.Duration(c.ConnectionIdleTimeoutInMs) * time.Millisecond,
 		Dial: func() (redis.Conn, error) {
-			return redis.Dial("tcp", c.Address)
+			return redis.Dial(
+				"tcp",
+				c.Address,
+				redis.DialConnectTimeout(time.Duration(c.ConnectTimeoutInMs)*time.Millisecond),
+				redis.DialReadTimeout(time.Duration(c.ReadTimeoutInMs)*time.Millisecond),
+				redis.DialWriteTimeout(time.Duration(c.WriteTimeoutInMs)*time.Millisecond),
+			)
 		},
 	}
 
@@ -25,25 +31,36 @@ func New(c Config) transport.Connection {
 }
 
 type Config struct {
+	// Address specifies the location of the redis sever and is used when dialing a connection.
 	Address string `default:"localhost:6379"`
 
-	Db int `default:"0"`
+	// DB specifies the database to select when dialing a connection.
+	DB int `default:"0"`
 
-	// Maximum number of idle connections in the pool.
-	MaxIdleConnections int `default:"2"`
+	// MaxIdleConnections is a maximum number of idle connections in the pool.
+	MaxIdleConnections int `default:"50"`
 
-	// Maximum number of connections allocated by the pool at a given time.
+	// MaxActiveConnections is a maximum number of connections allocated by the pool at a given time.
 	// When zero, there is no limit on the number of connections in the pool.
-	MaxActiveConnections int `default:"2"`
+	MaxActiveConnections int `default:"1000"`
 
-	// Close connections after remaining idle for this duration. If the value
-	// is zero, then idle connections are not closed. Applications should set
+	// ConnectionIdleTimeoutInMs close connections after remaining idle for this duration.
+	// If the value is zero, then idle connections are not closed. Applications should set
 	// the timeout to a value less than the server's timeout.
-	ConnectionIdleTimeoutInSeconds int `default:"120"`
+	// The default ConnectionIdleTimeoutInMs is 300000ms/300s/5m.
+	ConnectionIdleTimeoutInMs int `default:"300000"`
 
-	ReadTimeoutInSeconds int `default:"10"`
+	// ConnectTimeoutInMs specifies the timeout for connecting to the Redis server.
+	// The default ConnectTimeoutInMs is 10000ms/10s.
+	ConnectTimeoutInMs int `default:"10000"`
 
-	WriteTimeoutInSeconds int `default:"10"`
+	// ReadTimeoutInMs specifies the timeout for reading a single command reply.
+	// The default ReadTimeoutInMs is 10000ms/10s.
+	ReadTimeoutInMs int `default:"10000"`
+
+	// WriteTimeoutInMs specifies the timeout for writing a single command.
+	// The default WriteTimeoutInMs is 10000ms/10s.
+	WriteTimeoutInMs int `default:"10000"`
 }
 
 type message struct {
