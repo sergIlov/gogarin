5af75bcd718128c8c2f3252d3c4c1f03e990a467
Anton Kuzmenko <antonkuzmenko.dev@gmail.com>
2017-09-16 13:36:36 +0300

    Extract version variables to version.go

--- cmd/space_center/main.go
+++ cmd/space_center/main.go
@@ -18,13 +18,6 @@ import (
 	"github.com/kelseyhightower/envconfig"
 )
 
-// set by release script, see Makefile
-var (
-	version   = "dev"
-	commit    string
-	buildTime string
-)
-
 type Config struct {
 	RPC struct {
 		Adapter             string `required:"true"`
